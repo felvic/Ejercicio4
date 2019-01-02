034cf2b41925366ad97d70957d106515abcc6c0d
felvic <felrivicu@gmail.com>
2019-01-01 23:36:05 -0400

    agrege un alert y una concatenación

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 /* Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor. */
-
+alert("Escriba dos numeros para saber cual es mayor");
 var primerNum = prompt("Escribe el primer numero");
 var segundoNum = prompt("Ahora escribe el segundo numero");
 
 if (primerNum > segundoNum)
 {
-  document.write(primerNum);
+  document.write(primerNum + " es mayor");
 }
 else if (primerNum < segundoNum)
 {
-  document.write(segundoNum);
+  document.write(segundoNum + " es mayor");
 }
